96e6eedefa32d036372ed340c78d2d03d60ac4e0
Niaz264 <82967650+Niaz264@users.noreply.github.com>
2023-01-29 14:36:46 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
-$START_CMD
+git clone https://github.com/Niaz264/searchrrr ok && cd ok && pip3 install -U -r requirements.txt
+python3 -m bot
